6fd620df4b0c72feb66973dfc13d1df0e8edc1df
Anti20-creator <61777466+Anti20-creator@users.noreply.github.com>
2020-12-26 23:13:27 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ZH.ViewModel;
+
+namespace ZH
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Model _model;
+        GameViewModel _viewModel;
+        MainWindow _view;
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            _model = new Model();
+            _viewModel = new GameViewModel(_model);
+
+            _viewModel.StartGame += ViewModel_StartGame;
+
+            _view = new MainWindow();
+            _view.DataContext = _viewModel;
+            _view.Show();
+        }
+
+        private void ViewModel_StartGame(object sender, int e)
+        {
+            _model.StartGame(e);
+        }
+
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+    }
+}
